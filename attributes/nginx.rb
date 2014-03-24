bfc411f5063764804c2cf81f385170dac5d290a7
Gregoire Seux <g.seux@criteo.com>
2014-03-24 14:30:07 +0100

    Fixup

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -1,6 +1,6 @@
 default['grafana']['nginx']['template'] = 'grafana-nginx.conf.erb'
 default['grafana']['nginx']['template_cookbook'] = 'grafana'
 
-include_attributes 'nginx'
+include_attribute 'nginx'
 
 default['nginx']['enable_default_site'] = false

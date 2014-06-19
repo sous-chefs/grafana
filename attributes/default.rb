4ffe5d3288a04f0e3ff21de814d1f9832af08965
Greg Fitzgerald <greg@gregf.org>
2014-06-19 12:16:51 -0400

    Add some comments to attributes to help organize it

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,24 +10,31 @@ default['grafana']['file']['checksum'] = "603156ca2f051e5e42ec5c72d750f577b8f558
 default['grafana']['webserver'] = "nginx"
 default['grafana']['install_path'] = "/opt"
 default['grafana']['install_dir'] = "#{node['grafana']['install_path']}/grafana"
+
 case node['grafana']['install_type']
 when 'git'
   default['grafana']['web_dir'] = "#{node['grafana']['install_dir']}/current/src"
 when 'file'
   default['grafana']['web_dir'] = node['grafana']['install_dir']
 end
+
+# elastic search
 default['grafana']['es_server'] = "127.0.0.1"
 default['grafana']['es_port'] = "9200"
 default['grafana']['es_role'] = "elasticsearch_server"
 default['grafana']['es_scheme'] = "http://"
 default['grafana']['es_user'] = ''
 default['grafana']['es_password'] = ''
+
+# graphite
 default['grafana']['graphite_server'] = "127.0.0.1"
 default['grafana']['graphite_port'] = "80"
 default['grafana']['graphite_role'] = "graphite_server"
 default['grafana']['graphite_scheme'] = "http://"
 default['grafana']['graphite_user'] = ''
 default['grafana']['graphite_password'] = ''
+
+# webserver
 default['grafana']['user'] = ''
 default['grafana']['config_template'] = 'config.js.erb'
 default['grafana']['config_cookbook'] = 'grafana'
@@ -36,6 +43,8 @@ default['grafana']['webserver_aliases'] = [node['ipaddress']]
 default['grafana']['webserver_listen'] = node['ipaddress']
 default['grafana']['webserver_port'] = 80
 default['grafana']['webserver_scheme'] = "http://"
+
+# config.js
 default['grafana']['timezone_offset'] = "null" # Example: "-0500" (for UTC - 5 hours)
 default['grafana']['grafana_index'] = 'grafana-index'
 default['grafana']['unsaved_changes_warning'] = 'true'

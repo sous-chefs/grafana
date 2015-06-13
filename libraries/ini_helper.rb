7994a39b52af8abd3a38b96d57621c72b8690fbf
Jonathan Tron <jonathan@tron.name>
2015-06-13 10:25:08 +0200

    Fix ini config rendering (refs #56)

--- libraries/ini_helper.rb
+++ libraries/ini_helper.rb
@@ -1,18 +1,29 @@
 module GrafanaCookbook
   module IniHelper
+    def self.format_config(config)
+      output = []
+      config.each do |section, groups|
+        output << format_section(section)
+        groups.each do |key, value|
+          output << format_kv(key, value)
+        end
+      end
+      output.join "\n"
+    end
+
     def self.format_section(section)
       "[#{section}]" if section
     end
 
-    def self.format_value(value)
+    def self.format_kv(key, value)
       case value
       when Hash
         line = ''
         line << '# ' + value['comment'] + "\n" if value['comment']
         line << ';' if value['disable']
-        line << value['value']
+        line << "#{key} = #{value['value']}"
       else
-        value
+        "#{key} = #{value}"
       end
     end
   end

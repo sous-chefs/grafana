ea3afe815c6ed09597312d9cbe8535bdf573cb51
Jean Baptiste Favre <debian@jbfavre.org>
2015-10-03 19:31:58 +0200

    Add new matchers

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,4 +1,5 @@
 if defined?(ChefSpec)
+  # Data Source matchers
   def create_grafana_datasource(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_datasource, :create, name)
   end
@@ -6,27 +7,35 @@ if defined?(ChefSpec)
   def create_grafana_datasource_if_missing(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_datasource, :create_if_missing, name)
   end
-
   def delete_grafana_datasource(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_datasource, :delete, name)
   end
 
+  # Dashboard matchers
   def create_grafana_dashboard(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_dashboard, :create, name)
   end
-
   def create_grafana_dashboard_if_missing(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_dashboard, :create_if_missing, name)
   end
-
   def delete_grafana_dashboard(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_dashboard, :delete, name)
   end
 
-  def create_grafana_organization_if_missing(name)
-    ChefSpec::Matchers::ResourceMatcher.new(:grafana_organization, :create_if_missing, name)
+  # Organization matchers
+  def create_grafana_organization(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:grafana_organization, :create, name)
   end
 
+  def update_grafana_organization(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:grafana_organization, :update, name)
+  end
+
+  def delete_grafana_organization(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:grafana_organization, :delete, name)
+  end
+
+  # User matchers
   def create_grafana_user_if_missing(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_user, :create_if_missing, name)
   end

8c1aa7711a4c91b2f8f58dbfb179df3748fb0913
Jean Baptiste Favre <debian@jbfavre.org>
2015-10-03 19:45:20 +0200

    Update OrganizationApi

--- libraries/organization_api.rb
+++ libraries/organization_api.rb
@@ -3,13 +3,13 @@ module GrafanaCookbook
     include GrafanaCookbook::ApiHelper
 
     #
-    def add_org(name, grafana_options)
+    def add_org(organization, grafana_options)
       session_id = login(grafana_options[:host], grafana_options[:port], grafana_options[:user], grafana_options[:password])
       http = Net::HTTP.new(grafana_options[:host], grafana_options[:port])
-      request = Net::HTTP::Post.new('/api/org/')
+      request = Net::HTTP::Post.new('/api/orgs')
       request.add_field('Cookie', "grafana_user=#{grafana_options[:user]}; grafana_sess=#{session_id};")
       request.add_field('Content-Type', 'application/json;charset=utf-8;')
-      request.body = { 'name' => name }.to_json
+      request.body = organization.to_json
 
       response = with_limited_retry tries: 10, exceptions: Errno::ECONNREFUSED do
         http.request(request)
@@ -28,13 +28,13 @@ module GrafanaCookbook
     end
 
     #
-    def update_org(org_options, grafana_options)
+    def update_org(organization, grafana_options)
       session_id = login(grafana_options[:host], grafana_options[:port], grafana_options[:user], grafana_options[:password])
       http = Net::HTTP.new(grafana_options[:host], grafana_options[:port])
-      request = Net::HTTP::Put.new('/api/org')
+      request = Net::HTTP::Put.new('/api/orgs/' + organization[:id].to_s)
       request.add_field('Cookie', "grafana_user=#{grafana_options[:user]}; grafana_sess=#{session_id};")
       request.add_field('Content-Type', 'application/json;charset=utf-8;')
-      request.body = org_options.to_json
+      request.body = organization.to_json
 
       response = with_limited_retry tries: 10, exceptions: Errno::ECONNREFUSED do
         http.request(request)
@@ -52,11 +52,35 @@ module GrafanaCookbook
       nil
     end
 
+    #
+    def delete_org(organization, grafana_options)
+      session_id = login(grafana_options[:host], grafana_options[:port], grafana_options[:user], grafana_options[:password])
+      http = Net::HTTP.new(grafana_options[:host], grafana_options[:port])
+      request = Net::HTTP::Delete.new('/api/orgs/' + organization[:id].to_s)
+      request.add_field('Cookie', "grafana_user=#{grafana_options[:user]}; grafana_sess=#{session_id};")
+      request.add_field('Content-Type', 'application/json;charset=utf-8;')
+
+      response = with_limited_retry tries: 10, exceptions: Errno::ECONNREFUSED do
+        http.request(request)
+      end
+
+      handle_response(
+        request,
+        response,
+        success: 'Organization deleted',
+        unknown_code: 'OrganizationApi::delete_org unchecked response code: %{code}'
+      )
+
+      JSON.parse(response.body)
+    rescue BackendError
+      nil
+    end
+
     # Get a list of the existing organizations
     def get_orgs_list(grafana_options)
       session_id = login(grafana_options[:host], grafana_options[:port], grafana_options[:user], grafana_options[:password])
       http = Net::HTTP.new(grafana_options[:host], grafana_options[:port])
-      request = Net::HTTP::Get.new('/api/user/orgs')
+      request = Net::HTTP::Get.new('/api/orgs')
       request.add_field('Cookie', "grafana_user=#{grafana_options[:user]}; grafana_sess=#{session_id};")
       request.add_field('Accept', 'application/json')
 

48f46c71e73aec3938d73d1672a792086a1ed14b
Jean Baptiste Favre <debian@jbfavre.org>
2015-10-03 19:46:02 +0200

    Update organization resource & provider

--- providers/organization.rb
+++ providers/organization.rb
@@ -1,3 +1,5 @@
+require 'chef/mash'
+
 include GrafanaCookbook::OrganizationApi
 
 use_inline_resources if defined?(use_inline_resources)
@@ -15,13 +17,61 @@ action :create_if_missing do
   }
   orgs = get_orgs_list(grafana_options)
 
+  new_resource.organization[:name] = new_resource.name
   exists = false
   orgs.each do |org|
     exists = true if org['name'] == new_resource.name
   end
   unless exists
     converge_by("Creating organization #{new_resource.name}") do
-      add_org(new_resource.name, grafana_options)
+      add_org(new_resource.organization, grafana_options)
+    end
+  end
+end
+
+action :update do
+  grafana_options = {
+    host: new_resource.host,
+    port: new_resource.port,
+    user: new_resource.user,
+    password: new_resource.password
+  }
+  orgs = get_orgs_list(grafana_options)
+
+  exists = false
+  orgs.each do |org|
+    if org['name'] == new_resource.name
+      exists = true
+      new_resource.organization[:id] = org['id']
+    end
+  end
+  if exists
+    converge_by("Updating organization #{new_resource.name}") do
+      update_org(new_resource.organization, grafana_options)
+    end
+  end
+end
+
+action :delete do
+  grafana_options = {
+    host: new_resource.host,
+    port: new_resource.port,
+    user: new_resource.user,
+    password: new_resource.password
+  }
+  orgs = get_orgs_list(grafana_options)
+
+  Chef::Log.warn "Looking for organization #{new_resource.name}"
+  Chef::Log.warn "in " + orgs.to_s
+  exists = false
+  orgs.each do |org|
+    if org['name'] == new_resource.name
+      exists = true
+      Chef::Log.warn "Found organization " + org.to_s
+      new_resource.organization[:id] = org['id']
+      converge_by("Deleting organization #{new_resource.name}") do
+        delete_org(new_resource.organization, grafana_options)
+      end
     end
   end
 end
--- resources/organization.rb
+++ resources/organization.rb
@@ -1,8 +1,11 @@
-actions :create_if_missing
+actions :create_if_missing, :update, :delete
 default_action :create_if_missing
+
+state_attrs :organization_name
 
 attribute :host, kind_of: String, default: 'localhost'
 attribute :port, kind_of: Integer, default: 3000
 attribute :user, kind_of: String, default: 'admin'
 attribute :password, kind_of: String, default: 'admin'
-attribute :name, kind_of: String, name_attribute: true, required: true
+attribute :name, kind_of: String, required: true
+attribute :organization, kind_of: Hash, default: {}

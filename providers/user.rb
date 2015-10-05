9cb5b73544760e3f62fd2a409540ce9823e765e6
Jean Baptiste Favre <debian@jbfavre.org>
2015-10-05 21:34:09 +0200

    User provider rewrite
    Implements:
    - create_if_missing
    - update
    - delete

--- providers/user.rb
+++ providers/user.rb
@@ -1,3 +1,5 @@
+require 'chef/mash'
+
 include GrafanaCookbook::UserApi
 
 use_inline_resources if defined?(use_inline_resources)
@@ -10,28 +12,85 @@ action :create_if_missing do
   grafana_options = {
     host: new_resource.host,
     port: new_resource.port,
-    user: new_resource.user,
-    password: new_resource.password
+    user: new_resource.admin_user,
+    password: new_resource.admin_password
   }
-  user_options = {
-    name: new_resource.full_name,
-    email: new_resource.email,
-    login: new_resource.login,
-    password: new_resource.passwd
-  }
-  if new_resource.global
-    global_users = get_global_user_list(grafana_options)
+  users_list = get_user_list(grafana_options)
+  exists = false
 
-    exists = false
-    global_users.each do |user|
-      exists = true if user['login'] == new_resource.login
+  users_list.each do |user|
+    if user['login'] == new_resource.user[:login]
+      exists = true
     end
-    unless exists
-      converge_by("Creating global user #{new_resource.login}") do
-        add_global_user(user_options, grafana_options)
+    break if exists
+  end
+  unless exists
+    new_resource.user[:name] = new_resource.name
+    converge_by("Creating user #{new_resource.user[:login]}") do
+      add_user(new_resource.user, grafana_options)
+    end
+    converge_by("Setting permissions #{new_resource.user[:login]}") do
+      update_user_permission(new_resource.user, grafana_options)
+    end
+
+  end
+end
+
+action :update do
+  grafana_options = {
+    host: new_resource.host,
+    port: new_resource.port,
+    user: new_resource.admin_user,
+    password: new_resource.admin_password
+  }
+  users_list = get_user_list(grafana_options)
+  exists = false
+
+  if new_resource.user[:login] != new_resource.name
+    old_login = new_resource.name
+    new_login = new_resource.user[:login]
+  else
+    old_login = new_login = new_resource.user[:login]
+  end
+  users_list.each do |user|
+    if user['login'] == old_login
+      exists = true
+      new_resource.user[:id] = user['id']
+      converge_by("Updating details for user #{new_resource.user[:login]}") do
+        update_user_details(new_resource.user, grafana_options)
+      end
+      converge_by("Updating password for user #{new_resource.user[:login]}") do
+        update_user_password(new_resource.user, grafana_options)
+      end
+      if new_resource.user[:isAdmin] != user['isAdmin']
+        converge_by("Updating permissions for user #{new_resource.user[:login]}") do
+          update_user_permissions(new_resource.user, grafana_options)
+        end
       end
     end
-  else
-    Chef::Log.error 'Non-global user creation is not currently supported'
+    break if exists
+  end
+end
+
+action :delete do
+  grafana_options = {
+    host: new_resource.host,
+    port: new_resource.port,
+    user: new_resource.admin_user,
+    password: new_resource.admin_password
+  }
+  users_list = get_user_list(grafana_options)
+  exists = false
+
+  users_list.each do |user|
+    if user['login'] == new_resource.user[:login]
+      exists = true
+      new_resource.user[:id] = user['id']
+      new_resource.user[:name] = new_resource.name
+      converge_by("Deleting user #{new_resource.user[:login]}") do
+        delete_user(new_resource.user, grafana_options)
+      end
+    end
+    break if exists
   end
 end

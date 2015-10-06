59b690f2c2cf29ab595058bf9e90e40386fb4710
Jean Baptiste Favre <debian@jbfavre.org>
2015-10-06 14:33:01 +0200

    Update datasource resource

--- resources/datasource.rb
+++ resources/datasource.rb
@@ -1,11 +1,11 @@
-actions :create, :create_if_missing, :delete
+actions :create, :update, :delete
 default_action :create
 
-state_attrs :source_name
+state_attrs :name
 
 attribute :host, kind_of: String, default: 'localhost'
 attribute :port, kind_of: Integer, default: 3000
-attribute :user, kind_of: String, default: 'admin'
-attribute :password, kind_of: String, default: 'admin'
-attribute :source_name, kind_of: String, name_attribute: true, required: true
-attribute :source, kind_of: Hash, default: {}
+attribute :admin_user, kind_of: String, default: 'admin'
+attribute :admin_password, kind_of: String, default: 'admin'
+attribute :name, kind_of: String, name_attribute: true, required: true
+attribute :datasource, kind_of: Hash, default: {}

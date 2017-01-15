criteo-forks/chef-grafana

Add support for grafana plugins

libraries/matchers.rb
@@ -50,4 +50,17 @@ if defined?(ChefSpec)
   def delete_grafana_user(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_user, :delete, name)
   end
+
+  # Plugin matchers
+  def install_plugin(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:manage_plugin, name, :install)
+  end
+
+  def update_plugin(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:manage_plugin, name, :update)
+  end
+
+  def remove_plugin(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:manage_plugin, name, :remove)
+  end
 end

sous-chefs/grafana

libraries/matchers.rb
@@ -50,4 +50,17 @@ if defined?(ChefSpec)
   def delete_grafana_user(name)
     ChefSpec::Matchers::ResourceMatcher.new(:grafana_user, :delete, name)
   end
+
+  # Plugin matchers
+  def plugin_install_cmd(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:build_cli_cmd, name, :install)
+  end
+
+  def plugin_update_cmd(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:build_cli_cmd, name, :update)
+  end
+
+  def plugin_remove_cmd(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:build_cli_cmd, name, :remove)
+  end
 end
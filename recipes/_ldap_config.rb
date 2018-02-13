71b37d7f8445364739cc30c1158d700c182ffc8f
Rajiv <rajiv.g@introhive.com>
2018-02-13 12:38:34 +0530

    Load Grafana configuration from Databag

--- recipes/_ldap_config.rb
+++ recipes/_ldap_config.rb
@@ -1,9 +1,15 @@
 ldap = node['grafana']['ldap'].dup
 verbose_logging = node['grafana']['ldap_verbose_logging']
 
+data_bag_name = node['grafana']['data_bag']['name']
+config_item = node['grafana']['data_bag']['config_item']
+grafana_config = GrafanaCookbook::Helper.data_bag_item(data_bag_name, config_item, missing_ok=true)
+
+ini = Chef::Mixin::DeepMerge.merge(ldap, grafana_config)
+
 template node['grafana']['ini']['auth.ldap']['config_file']['value'] do
   source 'ldap.toml.erb'
-  variables verbose_logging: verbose_logging, config: ldap
+  variables verbose_logging: verbose_logging, config: ini
   owner 'root'
   group 'root'
   mode '0644'

90dae4d08d5a5c7a3f862e3a74b1854b2db507ac
Dan Webb <dan.webb@damacus.io>
2018-04-03 23:01:27 +0100

    Add CODEOWNERS

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,15 @@
-name 'grafana'
-maintainer 'Andrei Skopenko'
-maintainer_email 'andrei@skopenko.net'
-license 'Apache-2.0'
-description 'Installs/Configures Grafana Server'
+name             'grafana'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
+license          'Apache-2.0'
+description      'Installs/Configures Grafana Server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.2.1'
-source_url 'https://github.com/sous-chefs/chef-grafana'
-issues_url 'https://github.com/sous-chefs/chef-grafana/issues'
+source_url       'https://github.com/sous-chefs/chef-grafana'
+issues_url       'https://github.com/sous-chefs/chef-grafana/issues'
+chef_version     '>= 13.0'
+version          '2.2.1'
 
-chef_version '>= 13.0'
-
-supports 'ubuntu', '>= 12.04'
+supports 'ubuntu', '>= 14.04'
 supports 'debian', '>= 7.0'
 supports 'centos', '>= 6.4'
 

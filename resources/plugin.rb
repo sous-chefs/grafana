2014e3f662357bb1782fa700f37bdc712faf483e
Jason Field <jason@avon-lea.co.uk>
2018-12-27 18:05:31 +0000

    Update config resource
    Initial creation work
    
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>

--- resources/plugin.rb
+++ resources/plugin.rb
@@ -4,7 +4,7 @@
 #
 # Copyright 2014, Jonathan Tron
 # Copyright 2017, Andrei Skopenko
-# Copyright 2018, Sous Chefs 
+# Copyright 2018, Sous Chefs
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#property :grafana_cli_bin, String, default: '/usr/sbin/grafana-cli'
 
 default_action :install
 

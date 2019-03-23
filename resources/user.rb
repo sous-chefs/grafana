5dee47c879935e9948f75c15b6e471a7cb3e9cf2
Adam Grandquist <grandquista@gmail.com>
2019-03-23 08:11:49 -0700

    chef-client 14 freezes the new_resource hash
    * chef-client 14 freezes the new_resource hash

--- resources/user.rb
+++ resources/user.rb
@@ -39,37 +39,34 @@ action :create do
   }
   # If login is not provided as variable,
   # Let's use resource name for it
-  unless new_resource.user.key?(:login)
-    new_resource.user[:login] = new_resource.name
-  end
+  new_user = { login: new_resource.name }
+  new_user.merge!(new_resource.user)
 
   users_list = get_user_list(grafana_options)
   exists = false
 
   # Find wether user already exists
   users_list.each do |user|
-    exists = true if user['login'] == new_resource.user[:login]
+    exists = true if user['login'] == new_user[:login]
     break if exists
   end
 
   # If not found, let's create it and set its permissions
   unless exists
-    new_resource.user[:name] = new_resource.name
-    converge_by("Creating user #{new_resource.user[:login]}") do
-      add_user(new_resource.user, grafana_options)
+    new_user[:name] = new_resource.name
+    converge_by("Creating user #{new_user[:login]}") do
+      add_user(new_user, grafana_options)
     end
     new_users_list = get_user_list(grafana_options)
     new_users_list.each do |n_user|
-      if n_user['login'] == new_resource.user[:login]
-        new_resource.user[:id] = n_user['id']
-      end
+      n_user['login'] == new_user[:login] && new_user[:id] = n_user['id']
     end
-    converge_by("Setting permissions #{new_resource.user[:login]}") do
-      update_user_permissions(new_resource.user, grafana_options)
+    converge_by("Setting permissions #{new_user[:login]}") do
+      update_user_permissions(new_user, grafana_options)
     end
-    if new_resource.user.key?(:organizations)
-      converge_by("Adding user to organizations #{new_resource.user[:organizations].map { |org| org[:name] }}") do
-        add_user_to_orgs(new_resource.user, grafana_options)
+    if new_user.key?(:organizations)
+      converge_by("Adding user to organizations #{new_user[:organizations].map { |org| org[:name] }}") do
+        add_user_to_orgs(new_user, grafana_options)
       end
     end
   end
@@ -84,18 +81,17 @@ action :update do
   }
   # If login is not provided as variable,
   # Let's use resource name for it
-  unless new_resource.user.key?(:login)
-    new_resource.user[:login] = new_resource.name
-  end
+  new_user = { login: new_resource.name }
+  new_user.merge!(new_resource.user)
 
   users_list = get_user_list(grafana_options)
   exists = false
 
   # Check wether we have to update user's login
-  old_login = if new_resource.user[:login] != new_resource.name
+  old_login = if new_user[:login] != new_resource.name
                 new_resource.name
               else
-                new_resource.user[:login]
+                new_user[:login]
               end
 
   # Find wether user already exists
@@ -103,21 +99,21 @@ action :update do
   users_list.each do |user|
     if user['login'] == old_login
       exists = true
-      new_resource.user[:id] = user['id']
-      converge_by("Updating details for user #{new_resource.user[:login]}") do
-        update_user_details(new_resource.user, grafana_options)
+      new_user[:id] = user['id']
+      converge_by("Updating details for user #{new_user[:login]}") do
+        update_user_details(new_user, grafana_options)
       end
-      converge_by("Updating password for user #{new_resource.user[:login]}") do
-        update_user_password(new_resource.user, grafana_options)
+      converge_by("Updating password for user #{new_user[:login]}") do
+        update_user_password(new_user, grafana_options)
       end
-      if new_resource.user[:isAdmin] != user['isAdmin'] && !new_resource.user[:isAdmin].nil?
-        converge_by("Updating permissions for user #{new_resource.user[:login]}") do
-          update_user_permissions(new_resource.user, grafana_options)
+      if new_user[:isAdmin] != user['isAdmin'] && !new_user[:isAdmin].nil?
+        converge_by("Updating permissions for user #{new_user[:login]}") do
+          update_user_permissions(new_user, grafana_options)
         end
       end
-      if new_resource.user.key?(:organizations)
-        converge_by("Updating user organizations #{new_resource.user[:organizations].map { |org| org[:name] }}") do
-          update_user_orgs(new_resource.user, grafana_options)
+      if new_user.key?(:organizations)
+        converge_by("Updating user organizations #{new_user[:organizations].map { |org| org[:name] }}") do
+          update_user_orgs(new_user, grafana_options)
         end
       end
     end
@@ -134,9 +130,8 @@ action :delete do
   }
   # If login is not provided as variable,
   # Let's use resource name for it
-  unless new_resource.user.key?(:login)
-    new_resource.user[:login] = new_resource.name
-  end
+  new_user = { login: new_resource.name }
+  new_user.merge!(new_resource.user)
 
   users_list = get_user_list(grafana_options)
   exists = false
@@ -144,12 +139,12 @@ action :delete do
   # Find wether use already exists
   # If found, just delete it
   users_list.each do |user|
-    if user['login'] == new_resource.user[:login]
+    if user['login'] == new_user[:login]
       exists = true
-      new_resource.user[:id] = user['id']
-      new_resource.user[:name] = new_resource.name
-      converge_by("Deleting user #{new_resource.user[:login]}") do
-        delete_user(new_resource.user, grafana_options)
+      new_user[:id] = user['id']
+      new_user[:name] = new_resource.name
+      converge_by("Deleting user #{new_user[:login]}") do
+        delete_user(new_user, grafana_options)
       end
     end
     break if exists

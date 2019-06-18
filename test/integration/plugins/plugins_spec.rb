016055faaa1d51b23caf958b923a084037f3b794
Jason Field <jason@avon-lea.co.uk>
2019-06-18 22:11:53 +0100

    Fix plugins Tests

--- test/integration/plugins/plugins_spec.rb
+++ test/integration/plugins/plugins_spec.rb
@@ -1,13 +1,3 @@
 describe command('grafana-cli plugins ls') do
   its(:stdout) { should include 'grafana-clock-panel' }
 end
-
-describe http('http://localhost:3000/api/frontend/settings') do
-  its('status') { should eq 200 }
-
-  let(:json) { JSON.parse(subject.body) }
-  it { expect(json).to be_a Hash }
-
-  it { expect(json).to have_key 'panels' }
-  it { expect(json['panels']).to have_key 'grafana-clock-panel' }
-end

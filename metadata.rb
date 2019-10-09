c00c3f7d483f063827d7297d59b9bd8e0351afea
Jason Field <jason@avon-lea.co.uk>
2019-10-09 07:57:47 +0100

    Release 7.1.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures Grafana Server'
 source_url       'https://github.com/sous-chefs/chef-grafana'
 issues_url       'https://github.com/sous-chefs/chef-grafana/issues'
 chef_version     '>= 13.0'
-version          '7.0.0'
+version          '7.1.0'
 
 supports 'debian'
 supports 'ubuntu'

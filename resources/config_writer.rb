25ac2bc82e5a52e951e56eab4175bacc8ff78764
Jason Field <jason@avon-lea.co.uk>
2019-10-26 12:44:57 +0100

    Fix tests I hope

--- resources/config_writer.rb
+++ resources/config_writer.rb
@@ -1,13 +1,19 @@
-property  :instance_name,       String, name_property: true
-property  :is_sensitive,        [TrueClass, FalseClass], default: true
-property  :conf_directory,      String,                 default: '/etc/grafana'
-property  :config_file,         String,                 default: lazy { ::File.join(conf_directory, 'grafana.ini') }
-property  :config_file_ldap,    String,                 default: lazy { ::File.join(conf_directory, 'ldap.toml') }
-property  :cookbook,            String,                 default: 'grafana'
-property  :source,              String,                 default: 'grafana.ini.erb'
-property  :ldap_source,         String,                 default: 'ldap.toml.erb'
+property  :instance_name,       String,                   name_property: true
+property  :is_sensitive,        [TrueClass, FalseClass],  default: true
+property  :conf_directory,      String,                   default: '/etc/grafana'
+property  :config_file,         String,                   default: lazy { ::File.join(conf_directory, 'grafana.ini') }
+property  :config_file_ldap,    String,                   default: lazy { ::File.join(conf_directory, 'ldap.toml') }
+property  :cookbook,            String,                   default: 'grafana'
+property  :source,              String,                   default: 'grafana.ini.erb'
+property  :ldap_source,         String,                   default: 'ldap.toml.erb'
+property  :service_name,        String,                   default: 'grafana-server'
 
 action :install do
+  service new_resource.service_name do
+    action [:enable]
+    subscribes [:start, :restart], ['template[/etc/grafana/grafana.ini]', 'template[/etc/grafana/ldap.toml]'], :immediately
+  end
+
   template ::File.join(new_resource.config_file) do
     source new_resource.source
     cookbook new_resource.cookbook
--- test/fixtures/cookbooks/test/recipes/basic.rb
+++ test/fixtures/cookbooks/test/recipes/basic.rb
@@ -1,10 +1,5 @@
 grafana_install 'grafana'
 
-service 'grafana-server' do
-  action [:enable]
-  subscribes :restart, ['template[/etc/grafana/grafana.ini]', 'template[/etc/grafana/ldap.toml]'], :immediately
-end
-
 grafana_config 'Grafana'
 
 grafana_config_writer 'Grafana' do
